862a78f521ad80a368f9e2c2cdc0c16e3709948d
Jędrzej Dudkiewicz <jedrzej.dudkiewicz@zdalnyserwis.net.pl>
2021-05-31 19:38:10 +0200

    zestaw2 final version

--- zestaw2/src/main/scala/Main.scala
+++ zestaw2/src/main/scala/Main.scala
@@ -39,7 +39,7 @@ object Main extends App {
   }
   zad2;
   {
-  // Zadanie 3
+  // Zadanie 3 - FIXME: jako case class !  // FIXME: albo i nie.
   class Osoba (val imie: String, val nazwisko : String);
   def helloOsoba(o : Osoba) = (o.imie, o.nazwisko) match {
     case ("Adam", "A") => "Witaj, Janie A!";
@@ -68,27 +68,37 @@ object Main extends App {
     zad4;
   };
   {
-    class Osoba (val imie : String, val nazwisko : String, val podatek : Double);
-    trait Student {
-      val podatek = 0.0;
+    trait Opodatkowany {
+      def podatek : Double;
+    };
+    abstract class Osoba (val imie : String, val nazwisko : String) extends Opodatkowany {
+    };
+    trait Student extends Opodatkowany {
+      override def podatek = 0.0;
     }
-    trait Pracownik {
+    trait Pracownik extends Opodatkowany {
       private var _pensja : Double = 0.0;
       def pensja = _pensja;
       def pensja_= (v : Double) : Unit = _pensja = v;
-      def podatek = 0.2;
+      override def podatek = 0.2;
     };
     trait Nauczyciel extends Pracownik {
       override def podatek = 0.1;
     }
     def zad5 = {
       def test(o : Osoba) : Unit = println("Podatek dla " + o.imie + " " + o.nazwisko + " wynosi: " + o.podatek);
-      // Wszystko super, tylko jest jeden problem. w zadaniu jest napisane, że Osoba ma mieć podatek, a cała reszta ma go
-      // przesłaniać. I ekstra, tylko nie można w traicie przesłonić czegoś, czego jeszcze nie było, więc wydaje się, że nie
-      // można tego zrobić inaczej niż tylko przez przedefiniowanie w nowej klasie (anonimowej w poniższym przypadku)
-      // tej samej metody po raz kolejny. Nie brzmi to, jakby miało sens, ale może tak ma być
-      //test(new Osoba("Student", "S", 3.0) with Student);
+      test(new Osoba("Student", "S") with Student);
+      test(new Osoba("Pracownik", "P") with Pracownik);
+      test(new Osoba("Nauczyciel", "N") with Nauczyciel); 
+      test(new Osoba("StudPrac", "SP") with Student with Pracownik); // minusy studiowania w czasie pracy
+      test(new Osoba("PracStud", "PS") with Pracownik with Student); // bonusy pracy na studiach
+      test(new Osoba("NauStud", "NS") with Nauczyciel with Student); // Student nadpisał Mistrza
+      test(new Osoba("StudNaucz", "SN") with Student with Nauczyciel); // Nauczyciel nadpisał studenta
+      test(new Osoba("Mutant1", "M1") with Student with Nauczyciel with Pracownik); // zwraca 0.1 prawdopodobnie dlatego, że trait Pracownik jest w tym przypadku ciężko nadmiarowy - jset nadpisywany przez Nauczyciela
+      test(new Osoba("Mutant2", "M2") with Nauczyciel with Pracownik with Student); // a tutaj jest ok, Student jako ostatni nadpisuje wszystko wcześniej.
+      test(new Osoba("Mutant3", "M3") with Pracownik with Student with Nauczyciel);
     }
     zad5;
   };
 }
+
